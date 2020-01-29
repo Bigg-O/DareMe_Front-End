191cab8e5ef005c143e4dc556cfc2cb12337be5f
Wookeun Song <wookeun.s@gmail.com>
2020-01-28 20:21:33 -0600

    innitial components

---
+++ src/Containers/Body.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import OpenDareCard from "../Components/OpenDareCard";
+import CardColumns from "react-bootstrap/CardColumns";
+
+export class Body extends Component {
+  render() {
+    const {dares, onPay} = this.props
+    return (
+      <div>
+        <CardColumns className="col">
+          {dares.map(dare => 
+            <OpenDareCard key={dare.id} dare={dare} onPay={onPay}/>
+          )}
+        </CardColumns>
+      </div>
+    );
+  }
+}
+
+export default Body;
