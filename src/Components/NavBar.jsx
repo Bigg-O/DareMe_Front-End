9d2d2fb18998362dd9121717a383f862ad6e5ed5
Wookeun Song <wookeun.s@gmail.com>
2020-01-30 17:00:30 -0600

    navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,28 +1,36 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Navbar, Button, Badge, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { Navbar, Button, Badge, Nav, Image } from "react-bootstrap";
+import Logo from "../Images/DareMe_SM_Logo.png";
+import "./css/NavBar.css";
 
 export class NavBar extends Component {
   render() {
     const { wallet, username } = this.props.user;
     return (
-      <Navbar bg="primary" variant="dark" fixed="top">
-        <Navbar.Brand as={Link} to="/" className="title">
-          DareMe
+      <Navbar
+        collapseOnSelect
+        expand="md"
+        bg="primary"
+        variant="dark"
+        fixed="top"
+      >
+        <Navbar.Brand className="home-logo" as={NavLink} to="/">
+          <Image fluid src={Logo} />
         </Navbar.Brand>
-
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Button variant="primary">
-            Money: <Badge variant="light"> ${wallet}</Badge>
-          </Button>
-          <Navbar.Text>Signed in as: {username}</Navbar.Text>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <div className="mr-auto" />
+          <Nav>
+            {/* <Button variant="primary">
+              Money: <Badge variant="light"> ${wallet}</Badge>
+            </Button>
+            <Navbar.Text>Signed in as: {username}</Navbar.Text> */}
+            <Button className="add-dare" as={NavLink} to="/new_dare">
+              Add Dare!
+            </Button>
+          </Nav>
         </Navbar.Collapse>
-        <Nav.Link as={Link} to="/new_dare">
-          <Button className="dare_button" variant="secondary">
-            Add a Dare!
-          </Button>
-        </Nav.Link>
       </Navbar>
     );
   }
